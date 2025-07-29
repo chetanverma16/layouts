aee15f5fa08321fa8ae99fcb7062c96ec5ae6e2f
Chetan Verma <chetan61verma@gmail.com>
2025-07-29 14:36:54 +0530

    project: adding experiment-3 ai chat

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,19 @@
 export const data = [
   {
+    id: 3,
+    name: "AiChat.",
+    description:
+      "An AI-powered chat application layout built with Next.js and TailwindCSS, providing real-time messagingm built with Next.js and TailwindCSS, providing real-time messaging, and a sleek user interface.",
+    src: "https://experiment-3.vercel.app",
+    media: {
+      type: "video",
+      src: "/videos/experiment-3.webm",
+    },
+    github: "https://github.com/chetanverma16/experiment-3",
+    badge: "New",
+    techstack: ["Next.js", "TailwindCSS"],
+  },
+  {
     id: 2,
     name: "minbnb.",
     description:
@@ -10,7 +24,6 @@ export const data = [
       src: "/images/experiment-2.webp",
     },
     github: "https://github.com/webmakers-studio/experiment-2",
-    badge: "New",
     techstack: ["Next.js", "TailwindCSS"],
   },
   {
